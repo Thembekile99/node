8c2e2325ff052df000ddc0ac7578626fc812f13c
DA MAC M1 100 <damacm1100@DA-Mac100s-MacBook-Air.local>
2023-09-05 12:05:18 +0200

    step5

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -1,24 +1,52 @@
 const asyncHanlder = require('express-async-handler');
+const Todo = require('../models/todoModel');
+
+
+
 
 const getTodos = asyncHanlder(async (req, res) => {
-  res.status(200).json({ message: 'Get todos' })
-})
+  const todos = await Todo.find();
+  res.status(200).json(todos)
+});
 
-const setTodo = (req, res) => {
+const setTodo = asyncHanlder(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please add text field');
   }
-  res.status(200).json({ message: 'Set todo' })
-}
+  const todo = await Todo.create({
+    text: req.body.text,
+  });
+  res.status(200).json(todo);
+})
 
-const updateTodo = (req, res) => {
-  res.status(200).json({ message: `Update todo ${req.params.id}` })
-}
+const updateTodo = asyncHanlder(async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
 
-const deleteTodo = (req, res) => {
-  res.status(200).json({ message: `Delete todo ${req.params.id}` })
-}
+  if (!todo) {
+    res.status(400);
+    throw new Error('Todo not found');
+  }
+
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedTodo);
+})
+
+const deleteTodo = asyncHanlder(async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  if (!todo) {
+    res.status(400);
+    throw new Error('Todo not found');
+  }
+
+  await todo.remove();
+
+  res.status(200).json({ id: req.params.id });
+})
 
 module.exports = {
   getTodos,
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {getTodos, setTodo, updateTodo, deleteTodo} = require('../controllers/todosController')
 
-router.route('/').get(getTodos).post(setTodo)
-router.route('/:id').put(updateTodo).delete(deleteTodo)
+
+router.get('/get', getTodos)
+router.post('/post', getTodos)
+
+router.put('update/:id', updateTodo)
+router.delete('delete/:id',deleteTodo)
+
+
 
 module.exports = router;
